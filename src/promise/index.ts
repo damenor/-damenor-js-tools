423454c8ef0c6897aa8d4650b25ded5d1a9b773a
damenor <damenor.develop@gmail.com>
2022-03-31 18:52:35 +0200

    Include tryCatchWrapper in PROMISE

--- src/promise/index.ts
+++ src/promise/index.ts
@@ -10,6 +10,28 @@
  */
 const pause = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
+/**
+ * Wrapper for a promise including a try catch
+ * by default it shows error by console
+ * 
+ * Example:
+ * const promise = fetch(...)
+ * PROMISE.tryCatchWrapper(promise).then(...)
+ * 
+ * @param promise pause time in milliseconds
+ * @param onError optional ()
+ * @returns Promise
+ */
+ export const tryCatchWrapper = async (promise: Promise<any>, onError?: (error: any) =>  void) => {
+  try {
+    return await promise
+  } catch (error) {
+    if(onError) return onError(error)  
+    console.error({ error })
+  }
+}
+
 export const PROMISE = {
+  tryCatchWrapper,
   pause,
 }
